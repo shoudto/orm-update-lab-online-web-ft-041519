c602b5dc79103fe2c4772a4e0abd74b1b224b47e
Oscar Shoudt <shoudto@gmail.com>
2019-06-05 23:37:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
   
@@ -46,10 +47,22 @@ class Student
   end 
   
   def self.new_from_db(row)
-    new_student = self.new 
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    new_student = self.new(row[1], row[2], row[0])
+    # new_student.id = row[0]
+    # new_student.name = row[1]
+    # new_student.grade = row[2]
     new_student
+  end 
+  
+  def self.find_by_name(name)
+   # binding.pry 
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[1], result[2], result[0])
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
 end
